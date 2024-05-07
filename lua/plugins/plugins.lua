ab478abc9f3648fd266b852baa7544301a8184c8
Roberto Camejo <roberto.camejo@gmail.com>
2024-05-07 14:49:45 -0400

    First commit

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,46 @@
+local plugins = {
+  {
+    "kdheepak/lazygit.nvim",
+    	cmd = {
+    		"LazyGit",
+    		"LazyGitConfig",
+    		"LazyGitCurrentFile",
+    		"LazyGitFilter",
+    		"LazyGitFilterCurrentFile",
+    	},
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+           { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+      }
+    }
+  },
+  {
+  "neovim/nvim-lspconfig",
+    config = function ()
+      require "configs.lspconfig"
+    end
+  },
+}
+return plugins
